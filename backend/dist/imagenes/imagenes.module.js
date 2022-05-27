7860905983fa51360b4bc6867f46b0f215b0d0f5
Laura de Hoyos Zurdo <laura.hoyos@optimissa.com>
2022-05-27 14:24:53 +0200

    fallando app modules imports

--- backend/dist/imagenes/imagenes.module.js
+++ backend/dist/imagenes/imagenes.module.js
@@ -8,12 +8,16 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ImagenesModule = void 0;
 const common_1 = require("@nestjs/common");
+const typeorm_1 = require("@nestjs/typeorm");
+const inmuebles_entity_1 = require("../inmuebles/entities/inmuebles.entity");
+const imagenes_entity_1 = require("./entities/imagenes.entity");
 const imagenes_controller_1 = require("./imagenes.controller");
 const imagenes_service_1 = require("./imagenes.service");
 let ImagenesModule = class ImagenesModule {
 };
 ImagenesModule = __decorate([
     (0, common_1.Module)({
+        imports: [typeorm_1.TypeOrmModule.forFeature([imagenes_entity_1.ImagenesEntity]), typeorm_1.TypeOrmModule.forFeature([inmuebles_entity_1.InmueblesEntity])],
         controllers: [imagenes_controller_1.ImagenesController],
         providers: [imagenes_service_1.ImagenesService]
     })
--- backend/src/imagenes/imagenes.module.ts
+++ backend/src/imagenes/imagenes.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { InmueblesEntity } from 'src/inmuebles/entities/inmuebles.entity';
+import { ImagenesEntity } from './entities/imagenes.entity';
 import { ImagenesController } from './imagenes.controller';
 import { ImagenesService } from './imagenes.service';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([ImagenesEntity]),TypeOrmModule.forFeature([InmueblesEntity])],
   controllers: [ImagenesController],
   providers: [ImagenesService]
 })
