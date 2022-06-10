387bd5d3390575c018aee1fa1d480579d044135f
Laura de Hoyos Zurdo <laura.hoyos@optimissa.com>
2022-06-10 11:01:22 +0200

    mensajes de error en el front

--- frontend/src/components/Registro/Registro.js
+++ frontend/src/components/Registro/Registro.js
@@ -1,11 +1,16 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 function Registro (props){
+    const [mensajeError, setMensajeError] = useState("");
     
     const usuario = useRef(null);
     const correo = useRef(null);
     const contraseña = useRef(null);
     
+    // const usuarioContainer = useRef("");
+    // const mailContainer = useRef("");
+    // const contraseñaContainer = useRef("");
+
     function crearUsuario(e){
         e.nativeEvent.preventDefault();
 
@@ -17,16 +22,56 @@ function Registro (props){
                 'contraseña': contraseña.current.value,
                 'correo': correo.current.value
             })
+        }).then( result => {
+            return result.json();
+        }).then( result => {
+            if(result.message !== undefined){
+                
+                if(usuario.current.value === ""){
+                    console.log("user vacio")
+                    usuario.current.style.borderBottom = "3px solid red";
+                }else{
+                    usuario.current.style.borderBottom = "";
+                }
+        
+                if(correo.current.value === ""){
+                    console.log("correo vacio")
+                    correo.current.style.borderBottom = "3px solid red";
+                }else{
+                    correo.current.style.borderBottom = "";
+                }
+
+                if(contraseña.current.value === ""){
+                    console.log("contraeña vacio")
+                    contraseña.current.style.borderBottom = "3px solid red";
+                }else{
+                    contraseña.current.style.borderBottom = "";
+                }
+
+                if(result.message === "Ese usuario ya existe"){
+                    setMensajeError("Ese usuario ya existe");
+                }else{
+                    setMensajeError("");
+                }
+            }else{
+                console.log(2)
+                usuario.current.style.borderBottom = "";
+                correo.current.style.borderBottom = "";
+                contraseña.current.style.borderBottom = "";
+ 
+                fetch("http://localhost:8080/mails/welcome?email="+ correo.current.value + "&name=" + usuario.current.value, { 
+                    'method': 'POST',
+                    'headers': { 'Content-Type': 'application/json' } 
+                })
+                .catch(err => console.log('Solicitud fallida', err)); 
+                
+                setMensajeError("");
+                props.cerrarRegistro();
+            }
+          
         })
         .catch(err => console.log('Solicitud fallida', err));
         
-        fetch("http://localhost:8080/mails/welcome?email="+ correo.current.value + "&name=" + usuario.current.value, { 
-            'method': 'POST',
-            'headers': { 'Content-Type': 'application/json' } 
-        })
-        .catch(err => console.log('Solicitud fallida', err)); 
-
-        props.cerrarRegistro();
     }
 
 
@@ -53,14 +98,14 @@ function Registro (props){
                         <input type='text' className="registro-apellidos-caja"></input>
                     </div> */}
 
-                    <div className="registro-usuario">
+                    <div className="registro-usuario" >
                         <p className="registro-usuario-texto">Nombre de Usuario</p>
-                        <input ref={usuario} type='text' className="registro-usuario-caja"></input>
+                        <input ref={usuario} type='text' className="registro-usuario-caja" ></input>
                     </div>
 
                     <div className="registro-email">
                         <p className="registro-email-texto">Email</p>
-                        <input ref={correo} type='email' className="registro-email-caja"></input>
+                        <input ref={correo} type='email' className="registro-email-caja" ></input>
                     </div>
 
                     <div className="registro-contraseña">
@@ -81,6 +126,10 @@ function Registro (props){
                         <i className="fa-brands fa-facebook registro-redes-facebook"></i>
                         <i className="fa-brands fa-google-plus registro-redes-google"></i>
                     </div> */}
+
+                    <div className="acceder-mensaje-error">
+                        <p className="acceder-mensaje-error-texto">{mensajeError}</p>
+                    </div>
                     
                     <button className="registro-boton" onClick={(e)=>crearUsuario(e)}>Registrarse</button>
 
